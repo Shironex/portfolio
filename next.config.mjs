01d07a538167de69ced752b0d132c527391365ab
Kacper <kacperlachowiczwp.pl@wp.pl>
2024-10-16 23:50:24 +0200

    feat: add MDX components support and integrate markdown processing plugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
+import rehypePrism from '@mapbox/rehype-prism'
+import createMDX from '@next/mdx'
 import { createJiti } from 'jiti'
 import { fileURLToPath } from 'node:url'
+import rehypeStringify from 'rehype-stringify'
+import remarkGfm from 'remark-gfm'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
 
 const jiti = createJiti(fileURLToPath(import.meta.url))
 await jiti.import('./src/env/server.ts', { default: true })
@@ -10,6 +16,15 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 }
 
-export default nextConfig
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm, remarkParse, remarkRehype],
+    rehypePlugins: [rehypePrism, rehypeStringify],
+  },
+})
+
+export default withMDX(nextConfig)
