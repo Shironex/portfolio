4a9c6694bfed2e5bb255b3923113077fc83818fb
Kacper <kacperlachowiczwp.pl@wp.pl>
2025-03-21 02:37:49 +0100

    fix: eslint configuraiton

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,27 @@
+import { FlatCompat } from '@eslint/eslintrc'
 import nextPlugin from '@next/eslint-plugin-next'
 import tseslint from '@typescript-eslint/eslint-plugin'
-import tsparser from '@typescript-eslint/parser'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import checkFilePlugin from 'eslint-plugin-check-file'
 import nPlugin from 'eslint-plugin-n'
 
-export default [
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+const eslintConfig = [
   {
     ignores: ['node_modules/**', 'dist/**', '.github/**', 'pnpm-lock.yaml'],
   },
+  ...compat.config({
+    extends: ['next'],
+  }),
   {
     files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+    settings: {
+      next: {
+        rootDir: '.',
       },
     },
     plugins: {
@@ -25,6 +31,7 @@ export default [
       next: nextPlugin,
     },
     rules: {
+      // TypeScript rules
       'no-console': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
@@ -32,10 +39,14 @@ export default [
         'warn',
         { argsIgnorePattern: '^_' },
       ],
+
+      // Style rules
       'prefer-arrow-callback': ['error'],
       'prefer-template': ['error'],
       quotes: ['error', 'single'],
       'n/no-process-env': ['error'],
+
+      // File naming conventions
       'check-file/filename-naming-convention': [
         'error',
         {
@@ -51,9 +62,24 @@ export default [
           'src/**/!^[.*': 'KEBAB_CASE',
         },
       ],
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      ...nextPlugin.configs.typescript.rules,
+
+      // Next.js rules
+      'next/google-font-display': 'warn',
+      'next/google-font-preconnect': 'warn',
+      'next/no-css-tags': 'error',
+      'next/no-document-import-in-page': 'error',
+      'next/no-duplicate-head': 'error',
+      'next/no-head-import-in-document': 'error',
+      'next/no-html-link-for-pages': 'error',
+      'next/no-img-element': 'error',
+      'next/no-page-custom-font': 'error',
+      'next/no-sync-scripts': 'error',
+      'next/no-title-in-document-head': 'error',
+      'next/no-typos': 'error',
+      'next/no-unwanted-polyfillio': 'error',
     },
   },
   eslintConfigPrettier,
 ]
+
+export default eslintConfig
